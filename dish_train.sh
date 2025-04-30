e204e5792505133e2e21e12c12e962923032e9fc
sergiu <sergiudm@outlook.com>
2025-05-01 00:55:20 +0800

    Add training script for CNN model using torchrun

---
+++ dish_train.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+torchrun --nproc_per_node=4 train.py \
+    --seed 42 \
+    --epochs 35 \
+    --learning_rate 0.001 \
+    --batch_size 128 \
+    --num_workers 4 \
+    --model "CNN" \
+    --L1_parameter 0.00001 \
+    --L2_parameter 0.00001 \
+    --momentum 0.9 \
+    --full_dataset True
