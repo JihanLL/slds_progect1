694b92296e7ab6e7649fab1fb2513a052b1127c5
Sergiu <sergiudm@outlook.com>
2025-04-30 18:31:26 +0800

    Refactor output messages in training script for clarity and add training parameters to shell script

--- train.sh
+++ train.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python train.py
+python train.py \
+    --seed 42 \
+    --epochs 100 \
+    --learning_rate 0.001 \
+    --batch_size 32 \
+    --num_workers 4 \
+    --model "CNN" \
+    --L1_parameter 0.00001 \
+    --L2_parameter 0.00001 \
+    --momentum 0.9
